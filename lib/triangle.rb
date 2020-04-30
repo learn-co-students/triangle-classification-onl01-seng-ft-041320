6a4aefb1ee49d0a70c120aac0f738d452356a005
Stephen McBride <stevemcbride3@gmail.com>
2020-04-30 16:31:55 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+
+  attr_accessor :sides
+
+  def initialize(side1, side2, side3)
+    @sides = []
+    @sides.push(side1, side2, side3)
+  end
+
+  def kind
+    if sides.find {|side| side <= 0} || sides.sort.reverse[0] >= sides.sort.reverse[1..-1].sum
+      raise TriangleError
+    else
+      return :equilateral if sides.uniq.length == 1
+      return :isosceles if sides.uniq.length == 2
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "provided sides does not equate to a valid triangle."
+    end
+  end
 end
