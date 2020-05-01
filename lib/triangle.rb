6d394364030c5dc465be7bc9d7c91a44cb117a2b
Devone Austin <devone.austin@gmail.com>
2020-05-01 00:15:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+#Each argument is a length of one of the three sides of the triangle.
+
+  def initialize a, b, c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a <= 0 || b <= 0 || c <= 0 ||
+      a + b <= c || b + c <= a || a + c <= b
+    #  begin
+        raise TriangleError
+    #  rescue TriangleError => error
+    #      puts error.message
+    #  end
+    elsif a == b && b == c
+      :equilateral
+    elsif a == b || c == b || a == c
+      :isosceles
+    else a != b && c != b && a != c
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The sum of the length of any two sides of a triangle must be greater than the third."
+    end
+  end
 end
+
+
+#Triangle.new(0, -1, 1).kind
